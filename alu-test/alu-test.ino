37075a88b4c0dd610fab2f1a809968dd6c552dd4
marlinx <silvanmueller@gmx.ch>
2017-07-08 14:39:17 +0200

    add test for add 1+2=3

--- alu-test/alu-test.ino
+++ alu-test/alu-test.ino
@@ -1,23 +1,53 @@
 #include <ArduinoUnit.h>
 
-test(ok) 
-{
-  int x=3;
-  int y=3;
-  assertEqual(x,y);
+#define CARRY_IN 0
+#define A_BYTE 1
+#define B_BYTE 5
+
+#define SUM_BYTE 9
+#define CARRY_OUT 13
+
+void writeByte(int startPin, int value) {
+  for(int offset=0; offset < 4; offset++) {
+    int bitIsSet = (value >> offset) & 0x1;
+    digitalWrite(startPin+offset, bitIsSet ? HIGH : LOW);
+  }
 }
 
-test(bad)
+int readByte(int startPin) {
+  int value = 0;
+  for(int offset=0; offset < 4; offset++) {
+    value |= (digitalRead(startPin+offset) == HIGH ? 1 : 0) << offset;
+  }
+  return value;
+}
+
+test(add_one_two) 
 {
-  int x=3;
-  int y=3;
-  assertNotEqual(x,y);
+  digitalWrite(CARRY_IN, 0);
+  writeByte(A_BYTE, 1);
+  writeByte(B_BYTE, 2);
+  
+  int result = readByte(SUM_BYTE);
+  assertEqual(3, result);
+
+  int carry = digitalRead(CARRY_OUT);
+  assertEqual(LOW, carry);
 }
 
 void setup()
 {
   Serial.begin(9600);
   while(!Serial); // for the Arduino Leonardo/Micro only
+
+  for(int pin=0; pin <= 8; pin++) {
+      pinMode(pin, OUTPUT);
+  }
+
+  for(int pin=9; pin <= 13; pin++) {
+      pinMode(pin, INPUT);
+  }
+
 }
 
 void loop()
