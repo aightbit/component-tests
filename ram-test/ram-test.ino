88c16125f2429b5385366fedd9f1a7950853c9d5
marlinx <silvanmueller@gmx.ch>
2018-01-20 16:24:48 +0100

    implement test for ram component

--- ram-test/ram-test.ino
+++ ram-test/ram-test.ino
@@ -1,42 +1,13 @@
 #include <ArduinoUnit.h>
 
-
-#define CLK 50
-
 #define BUS 22
 
-#define LDR_A 42
-#define LDR_B 43
-#define ADD 46
-#define NEG 47
-#define OUT_A 36
-#define OUT_B 37
+#define CLK 40
 
-#define ADDR_BUS 2
+#define ADDR 42
+
 #define RAM_CS 50
 #define RAM_WE 52
-
-void setup()
-{
-  Serial.begin(9600);
-  while(!Serial); // for the Arduino Leonardo/Micro only
-
-  pinMode(CLK, OUTPUT);
-
-  for(int pin=BUS; pin < BUS+8; pin++) {
-      pinMode(pin, OUTPUT);
-  }
-  
-  pinMode(LDR_A, OUTPUT);
-  pinMode(LDR_B, OUTPUT);
-  pinMode(ADD, OUTPUT);
-  pinMode(NEG, OUTPUT);
-  pinMode(OUT_A, OUTPUT);
-  pinMode(OUT_B, OUTPUT);
-
-  pinMode(RAM_CS, OUTPUT);
-  pinMode(RAM_WE, OUTPUT);
-}
 
 void writeByte(int startPin, int value) {
   for(int offset=0; offset < 8; offset++) {
@@ -44,7 +15,7 @@ void writeByte(int startPin, int value) {
     pinMode(startPin+offset, OUTPUT);
     digitalWrite(startPin+offset, bitIsSet ? HIGH : LOW);
   }
-//  delay(1);
+  delay(1);
 }
 
 int readByte(int startPin) {
@@ -53,7 +24,7 @@ int readByte(int startPin) {
     pinMode(startPin+offset, INPUT);
     value |= (digitalRead(startPin+offset) == HIGH ? 1 : 0) << offset;
   }
-//  delay(1);
+  delay(1);
   return value;
 }
 
@@ -67,12 +38,12 @@ void writeBus(int value) {
 
 void high(int pin) {
   digitalWrite(pin, HIGH);
-//  delay(1);
+  delay(1);
 }
 
 void low(int pin) {
   digitalWrite(pin, LOW);
-//  delay(1);
+  delay(1);
 }
 
 void tick() {
@@ -80,42 +51,76 @@ void tick() {
   low(CLK);
 }
 
+void resetControlLinesAndBus() {
+  low(CLK);
+  high(ADDR);
+  high(RAM_CS);
+  high(RAM_WE);
+  
+  writeBus(0);
+}
+
+
+void setup()
+{
+  Serial.begin(9600);
+  while(!Serial); // for the Arduino Leonardo/Micro only
+
+  pinMode(CLK, OUTPUT);
+
+  pinMode(ADDR, OUTPUT);
+
+  pinMode(RAM_CS, OUTPUT);
+  pinMode(RAM_WE, OUTPUT);
+
+  for(int pin=BUS; pin < BUS+8; pin++) {
+      pinMode(pin, OUTPUT);
+  }
+
+  resetControlLinesAndBus();
+}
+
+
 void loop()
 {
 //  Test::run();
 
-    for(int i = 0; i < 0xFF; i++) {
-      // disable all
-      high(RAM_CS);
-      high(RAM_WE);
-      writeBus(0);
+  for(int i = 0; i < 0xFF; i++) {
+    resetControlLinesAndBus();
+    
+    low(ADDR);
+    writeBus(i);
+    tick();
+    high(ADDR);
 
-      int value = i;
-      writeByte(ADDR_BUS, i);
-      low(RAM_WE);
-      low(RAM_CS);
-      writeBus(value);
-      Serial.print("STR["); Serial.print(i); Serial.print("] = ");
-      Serial.println(value);
-      high(RAM_CS);
-      high(RAM_WE);
+    int value = i;
+    low(RAM_WE);
+    low(RAM_CS);
+    writeBus(value);
+    Serial.print("STR["); Serial.print(i); Serial.print("] = ");
+    Serial.println(value);
+    high(RAM_CS);
+    high(RAM_WE);
+  }
+
+  for(int i = 0; i < 0xFF; i++) {
+    resetControlLinesAndBus();
+
+    low(ADDR);
+    writeBus(i);
+    tick();
+    high(ADDR);
+
+    // just to make sure that we're not re-reading our written data
+    resetControlLinesAndBus();
+
+    low(RAM_CS);
+    int storedValue = readBus();
+    Serial.print("LDR["); Serial.print(i,BIN); Serial.print("] = ");
+    Serial.println(storedValue, BIN);
+    if(storedValue != i) {
+      Serial.println("wrong");
     }
-
-    for(int i = 0; i < 0xFF; i++) {
-//       disable all
-      high(RAM_CS);
-      high(RAM_WE);
-      writeBus(0);
-
-      writeByte(ADDR_BUS, i);
-      low(RAM_CS);
-      int storedValue = readBus();
-      Serial.print("LDR["); Serial.print(i); Serial.print("] = ");
-      Serial.println(storedValue);
-      if(storedValue != i) {
-        Serial.println("wrong");
-      }
-      high(RAM_CS);
-    }
-
+    high(RAM_CS);
+  }
 }
